bc99e789800253043c378722e68e14c86ee224e3
Luka <lukatakatoh7@gmail.com>
2022-09-29 08:59:38 +0900

    3

---
+++ Objects/player.py
@@ -0,0 +1,9 @@
+import pygame
+class Player:
+    def __init__(self, screen, spawn_x, spawn_y, image):
+        self.screen=screen
+        self.spawn_x=spawn_x
+        self.spawn_y=spawn_y
+        self.image=self.image
+    def draw(self):
+        pygame.image.load(self.image)
