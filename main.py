b9906ae02d5157989dd287368b0e7f62c5d3af2a
Luka <lukatakatoh7@gmail.com>
2022-09-29 08:44:49 +0900

    1

--- main.py
+++ main.py
@@ -5,9 +5,6 @@ pygame.init()
 from config import res
 
 
-<<<<<<< HEAD
-screen=pygame.display.set_mode(res, pygame.RESIZABLE)
+screen=pygame.display.set_mode(res)
 HomeScreen(screen)
-=======
-#hi i made a change
->>>>>>> 0c377003bf0d53cd69e9d432296c394249dbd79d
+
