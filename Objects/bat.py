81993e0e00fd995ff5fd20bd2c2830910716e81a
Luka <lukatakatoh7@gmail.com>
2023-04-27 08:51:36 +0900

    finished bat

--- Objects/bat.py
+++ Objects/bat.py
@@ -11,12 +11,14 @@ class Bat(pygame.sprite.Sprite):
         self.image_left=pygame.transform.flip(self.image, True, False)
         self.main_image=self.image
         self.rect = self.image.get_rect(topleft=location)
+        self.rect2=pygame.Rect([self.rect[0]-1, self.rect[1]-1, self.rect[2]+2, self.rect[3]+3])
         self.original_speed=speed
         self.speed = speed
         self.spawn_point=location
         self.position = pygame.math.Vector2((self.rect.x, self.rect.y))
     def update(self, player):
         self.move(player)
+        self.hurt_player(player)
     def check_collisions(self, offset, index, obstacles):
         
         unaltered = True
@@ -33,6 +35,17 @@ class Bat(pygame.sprite.Sprite):
 
         self.position += velocity
         self.rect.topleft = self.position
+    def hurt_player(self, player):
+        #print(player.on_moving,pygame.sprite.collide_rect(self,player))
+        
+        #print(self.rect2.colliderect(player.rect), 123)
+    
+        #print(player.rect.topleft)
+        if player.on_moving is self or pygame.sprite.collide_rect(self,player) or self.rect2.colliderect(player.rect):
+            
+            #print(player.check_collisions(offset, axis, obstacles))
+
+            player.hurt_player(15)
     def draw(self, surface):
         
         surface.blit(self.main_image, self.rect)
